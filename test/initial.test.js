21359af47307d26db481e1aa5c9ff2370b657cbb
Rafael Heredia Bastidas <heredia.developer@gmail.com>
2020-12-01 00:37:40 -0300

    jest configuration for commonjs

---
+++ test/initial.test.js
@@ -0,0 +1,15 @@
+const printNumber = require("../src/printNumber.js");
+
+describe("printNumber test", () => {
+    describe("this is the first printNumber", () => {
+        it('it will print a value', () => {
+            //arrange
+
+            //act
+            printNumber(10);
+
+            //assert
+            expect(1).toBe(2);
+        });
+    });
+});
